17dc9a8e7a1dfc83530f8e1d1d43c589cc669da1
SilentImp <thesilentimp@gmail.com>
2015-02-14 03:01:37 +0200

    changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express           = require('express')
     , app             = express()
-    , Git             = require('git-wrapper2')
-    , git             = new Git()
-    , gith            = require('gith').create(9001)
+    // , Git             = require('git-wrapper2')
+    // , git             = new Git()
+    // , gith            = require('gith').create(9001)
     , port            = process.env.PORT || 8080
     , multiparty      = require('connect-multiparty')
     , BellyController = require('./controllers/BellyController.js');
@@ -23,9 +23,13 @@ app.listen(port);
 console.log('Magic happens on http://178.79.181.157:' + port);
 
 // Auto Deploy
-gith({
-  repo: 'SilentImp/90daysofbelly'
-}).on( 'all', function( payload ) {
-  console.log( 'Post-receive happened!' );
-  git.pull('git@github.com:SilentImp/90daysofbelly.git', 'server');
-});
+// gith({
+//   repo: 'SilentImp/90daysofbelly'
+// }).on( 'all', function( payload ) {
+//   console.log( 'Post-receive happened!' );
+//   git.pull('git@github.com:SilentImp/90daysofbelly.git', 'server');
+// });
+
+hookshot()
+.on('refs/heads/server', 'git pull')
+.listen(9001)

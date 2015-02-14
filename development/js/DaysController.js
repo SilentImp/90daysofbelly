34182b304852d8d60f0769baff3491eda3d63099
SilentImp <thesilentimp@gmail.com>
2015-02-14 23:50:55 +0200

    script

--- development/js/DaysController.js
+++ development/js/DaysController.js
@@ -22,7 +22,7 @@
           file: file
         }).progress(function (evt) {
           var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-          $('.photo__invert').css({height: progressPercentage + '%'});
+          $('.upload__progress__invert').css({height: progressPercentage + '%'});
           console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
         }).success(function (data, status, headers, config) {
           console.log('file ' + config.file.name + 'uploaded. Response: ' + data);

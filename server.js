a620c563cc60ec3c978f49694a8c2687b0c05c2c
SilentImp <thesilentimp@gmail.com>
2015-02-14 22:44:22 +0200

    server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express           = require('express')
     , app             = express()
-    , router          = express.Router()
     , hookshot        = require('hookshot')
     , port            = process.env.PORT || 8080
     , multiparty      = require('connect-multiparty')
@@ -13,8 +12,8 @@ var express           = require('express')
 
 
 // Serving static
-router.use(express.static('public', options));
-router.use(function (req, res, next) {
+app.use(express.static('public', options));
+app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Origin',  'http://silentimp.github.io');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -22,18 +21,17 @@ router.use(function (req, res, next) {
 });
 
 // Notes
-router.get('/note/', BellyController.getNote);
-router.post('/note/', BellyController.saveNote);
+app.get('/note/', BellyController.getNote);
+app.post('/note/', BellyController.saveNote);
 
 // Weight
-router.get('/weight/',   BellyController.getWeight);
-router.post('/weight/',  BellyController.saveWeight);
+app.get('/weight/',   BellyController.getWeight);
+app.post('/weight/',  BellyController.saveWeight);
 
 // Photo
-router.put('/photo/', multiparty(), BellyController.uploadFile);
+app.put('/photo/', multiparty(), BellyController.uploadFile);
 
 // Server
-app.use('/', router);
 app.listen(port);
 console.log('Magic happens on http://178.79.181.157:' + port);
 

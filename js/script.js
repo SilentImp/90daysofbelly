a592dbe8ef9e06e67d8d1fff2c1f3988cb3f83f8
SilentImp <thesilentimp@gmail.com>
2015-02-14 21:53:37 +0200

    Update 2015-02-14T19:53:34.605Z

--- js/script.js
+++ js/script.js
@@ -1,28 +1,76 @@
 (function() {
 
-    var app = angular.module("BellyDays", []);
+    var app = angular.module("BellyDays", ['angularFileUpload']);
 
-    app.controller("AppController", function($scope) {
-        console.log('app', arguments);
-    });
+    app.controller("AppController", ['$scope', '$upload', function($scope, $upload) {
 
-    app.controller("DaysController", function($scope) {
+        $scope.page = 'list';
+        $scope.weight = null;
 
-        console.log('days', arguments);
-    });
+        $scope.$watch('files', function() {
+            $scope.upload($scope.files);
+        });
+
+        $scope.upload = function(files, event) {
+            if (files && files.length) {
+                console.log('files changed', arguments);
+                var file = files[0];
+                $upload.upload({
+                    url: 'http://178.79.181.157:8080/photo/',
+                    method: 'PUT',
+                    fields: {
+                        'username': $scope.username
+                    },
+                    file: file
+                }).progress(function(evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                }).success(function(data, status, headers, config) {
+                    console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                });
+            }
+        };
+
+        $scope.isWeight = function() {
+            return $scope.weight === null;
+        };
+
+        $scope.show = function(page) {
+            console.log('show page: ', page);
+            $scope.page = page;
+        };
+
+        $scope.openWeightForm = function() {
+            $scope.show('weight-form');
+            document.querySelector('.weight-form input').focus();
+        };
+
+        $scope.openNoteForm = function() {
+            $scope.show('note-form');
+            document.querySelector('.note-form textarea').focus();
+        };
+
+        // console.log('app', arguments);
+
+    }]);
 
     app.controller("NoteController", function($scope) {
-        console.log('note', arguments);
+        // console.log('note', arguments);
     });
 
     app.controller("NoteFormController", function($scope) {
-        console.log('note-form', arguments);
+        // console.log('note-form', arguments);
     });
 
     app.controller("WeightFormController", function($scope) {
-        console.log('weight-form', arguments);
+        $scope.addWeight = function() {
+            $scope.$parent.weight = $scope.weight;
+            document.querySelector('.weight-form').reset();
+            $scope.show('list');
+        };
+        // console.log('weight-form', arguments);
     });
 
 })();
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkRheXNDb250cm9sbGVyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoic2NyaXB0LmpzIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkRheXNDb250cm9sbGVyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJzY3JpcHQuanMifQ==

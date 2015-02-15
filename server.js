15580786f30413815b9ea418d93bfcdb9a824708
SilentImp <thesilentimp@gmail.com>
2015-02-15 18:01:06 +0200

    body

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Origin',  'http://silentimp.github.io');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  express.json();
-  express.urlencoded();
+  bodyParser.json();
+  bodyParser.urlencoded({
+    extended: true
+  });
   next();
 });
 
